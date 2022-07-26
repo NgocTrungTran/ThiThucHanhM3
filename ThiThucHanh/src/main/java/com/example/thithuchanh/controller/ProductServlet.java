a7a6f0c4e2d68705c649aafa0eab51c270ed8e22
NgocTrungTran <milotran1496@gmail.com>
2022-07-26 10:58:47 +0700

    Thi

---
+++ ThiThucHanh/src/main/java/com/example/thithuchanh/controller/ProductServlet.java
@@ -0,0 +1,163 @@
+package com.example.thithuchanh.controller;
+
+import com.example.thithuchanh.dao.IProductDao;
+import com.example.thithuchanh.dao.ProductDAO;
+import com.example.thithuchanh.model.Product;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.*;
+import java.util.function.BiConsumer;
+
+@WebServlet(name = "ProductServlet", urlPatterns = "/products")
+public class ProductServlet extends HttpServlet {
+    private IProductDao productDao;
+    private String errors = "";
+
+    public void init() {
+        productDao = new ProductDAO ();
+    }
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType ( "text/html;charset=UTF-8" );
+        req.setCharacterEncoding ( "utf-8" );
+        String action = req.getParameter ( "action" );
+        if ( action == null ) {
+            action = "";
+        }
+
+        try {
+            switch (action) {
+                case "add":
+                    showAddForm ( req, resp );
+                    break;
+                case "edit":
+                    showEditForm(req, resp);
+                    break;
+                case "delete":
+                    deleteProduct ( req, resp );
+                    break;
+                case "search":
+//                    searchAllField ( request, response );
+                    break;
+                default:
+                    listAllProduct ( req, resp );
+                    break;
+            }
+        } catch (SQLException ex) {
+            throw new ServletException ( ex );
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+        req.setCharacterEncoding("utf-8");
+        String action = req.getParameter("action");
+        if (action == null) {
+            action = "";
+        }
+        try {
+            switch (action) {
+                case "add":
+                    insertProduct(req, resp);
+                    break;
+                case "edit":
+                    updateProduct ( req, resp );
+                    break;
+                case "search":
+//                    listUserSearch(request, response);
+                    break;
+            }
+        } catch (SQLException ex) {
+            throw new ServletException(ex);
+        }
+    }
+
+    private void listAllProduct(HttpServletRequest request, HttpServletResponse response)
+            throws SQLException, IOException, ServletException {
+
+        ProductDAO dao = new ProductDAO ();
+        List<Product> listProduct = dao.selectAllProduct();
+        request.setAttribute ( "listProduct", listProduct );
+        RequestDispatcher view = request.getRequestDispatcher ( "/WEB-INF/product/list.jsp" );
+        view.forward ( request, response );
+    }
+    private void showAddForm(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+//        System.out.println ( "Servlet path: " + this.getServletContext ().getRealPath ( "/" ) );
+//        List<Product> listProduct = productDao.selectAllProduct ();
+//        request.setAttribute ( "listProduct", listProduct );
+        RequestDispatcher dispatcher = request.getRequestDispatcher ( "/WEB-INF/product/add.jsp" );
+        dispatcher.forward ( request, response );
+    }
+    private void showEditForm(HttpServletRequest request, HttpServletResponse response)
+            throws SQLException, ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+//        User existingUser = userDAO.selectUser(id);
+        Product selectProduct = productDao.selectProduct ( id );
+        request.setAttribute("id", selectProduct.getId ());
+        request.setAttribute("name", selectProduct.getName ());
+        request.setAttribute("price", selectProduct.getPrice ());
+        request.setAttribute("quantity", selectProduct.getQuantity ());
+        request.setAttribute("color", selectProduct.getColor ());
+        request.setAttribute("descri", selectProduct.getDescribe ());
+        request.setAttribute("category", selectProduct.getCategory ());
+
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/product/edit.jsp");
+
+        dispatcher.forward(request, response);
+    }
+    private void insertProduct(HttpServletRequest request, HttpServletResponse response)
+            throws SQLException, IOException, ServletException {
+        String name = request.getParameter ( "name" );
+        int price = Integer.parseInt ( request.getParameter ( "price" ) );
+        int quantity = Integer.parseInt ( request.getParameter ( "quantity" ) );
+        String color = request.getParameter ( "color" );
+        String descri = request.getParameter ( "descri" );
+        String category = request.getParameter ( "category" );
+
+        Product newProduct = new Product ( name, price, quantity, color, descri, category );
+        productDao.insertProduct ( newProduct );
+        RequestDispatcher dispatcher = request.getRequestDispatcher ( "/WEB-INF/product/add.jsp" );
+        dispatcher.forward ( request, response );
+        response.sendRedirect ( "/products" );
+    }
+    private void updateProduct(HttpServletRequest request, HttpServletResponse response)
+            throws SQLException, IOException, ServletException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        String name = request.getParameter ( "name" );
+        int price = Integer.parseInt ( request.getParameter ( "price" ) );
+        int quantity = Integer.parseInt ( request.getParameter ( "quantity" ) );
+        String color = request.getParameter ( "color" );
+        String descri = request.getParameter ( "descri" );
+        String category = request.getParameter ( "category" );
+
+        Product book = new Product (id, name, price, quantity, color, descri, category);
+        productDao.editProduct (book);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/products");
+        dispatcher.forward(request, response);
+    }
+    private void deleteProduct(HttpServletRequest request, HttpServletResponse response)
+            throws SQLException, IOException, ServletException {
+        int id = Integer.parseInt ( request.getParameter ( "id" ) );
+        productDao.deleteProduct ( id );
+
+        List<Product> listProduct = productDao.selectAllProduct ();
+        request.setAttribute ( "listProduct", listProduct );
+//        RequestDispatcher dispatcher = request.getRequestDispatcher ( "/products" );
+//        dispatcher.forward ( request, response );
+        response.sendRedirect ( "/products" );
+    }
+}
---
+++ ThiThucHanh/src/main/webapp/WEB-INF/layout/footer.jsp
@@ -0,0 +1,16 @@
+<footer class="footer">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-6">
+        07 - 2022 © Tran Ngoc Trung <a href="https://vi.wikipedia.org/wiki/%C4%90%E1%BB%93ng_H%E1%BB%9Bi" style="list-style: none"> Dong Hoi, Quang Binh</a>
+      </div>
+      <div class="col-md-6">
+        <div class="text-md-right footer-links d-none d-sm-block">
+          <a href="https://www.facebook.com/milo.tran.1800">About Us</a>
+          <a href="https://www.facebook.com/milo.tran.1800">Help</a>
+          <a href="https://www.facebook.com/milo.tran.1800">Contact Us</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
